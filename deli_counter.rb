ab5f136a0e34159da9907673bfa9120aa933eeb7
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 13:50:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,12 @@ def line(katz_deli)
   # if katz_deli.size == 0
   #   puts "The line is currently empty."
   # else
+    guests = []
     katz_deli.each_with_index do |name, index|
       place = index+1
-      puts "The line is currently: #{place}. #{name}"
+      guest << " #{place}. #{name}"
     end
+    puts "The line is currently:#{guest.join}"
   # end
 end
 def take_a_number(katz_deli, name)
