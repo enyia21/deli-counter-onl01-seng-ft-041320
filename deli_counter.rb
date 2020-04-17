a6079eee4824a54e55845b358f1f5d6a228dbb9c
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 13:04:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.sizeS} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   return katz_deli
 end
