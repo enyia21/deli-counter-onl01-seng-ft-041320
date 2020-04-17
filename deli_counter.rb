f6733e585bd00bc12f0932fb5c1149e9bb910fde
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 13:52:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 # Write your code here.
 def line(katz_deli)
-  # if katz_deli.size == 0
-  #   puts "The line is currently empty."
-  # else
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
     guests = []
     katz_deli.each_with_index do |name, index|
       place = index+1
       guests << " #{place}. #{name}"
     end
     puts "The line is currently:#{guests.join}"
-  # end
+  end
 end
 def take_a_number(katz_deli, name)
   katz_deli << name
