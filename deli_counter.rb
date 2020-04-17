101586d14862062451f9b59eca4194fc8cb360b5
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 14:06:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,9 +18,9 @@ def take_a_number(katz_deli, name)
   return katz_deli
 end
 def now_serving(katz_deli)
-  if katz_deli.size == 0 
+  if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
-  else  
+  else
     "Currenty serving #{katz_deli.shift!}"
   end
 end
