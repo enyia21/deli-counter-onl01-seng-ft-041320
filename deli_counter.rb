23e0c91b7e3e9e28b4b7feb294eb4645cefd121a
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 13:34:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,13 @@
 # Write your code here.
-
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+      puts "The line is currently: #{index + 1}. #{name}"
+    end
+  end
+end
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
